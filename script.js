551f1fc92691eb6bfa6bfed4542a7ba4247daf52
Taylor Kaldahl <tkaldahl90@gmail.com>
2018-05-30 16:56:40 -0400

    committing a working version

--- script.js
+++ script.js
@@ -17,6 +17,8 @@ var currentDisk = null
 
 var selectDisk = function () {
   currentDisk = this
+  // currentDisk = document.getElementById(topDiskId)
+  console.log(currentDisk)
 }
 diskEvents()
 // Adds event listeners to disks so they can be selected one at a time using the var currentDisk.
@@ -29,15 +31,35 @@ var pegEvents = function () {
   }
 }
 
+var topDiskId = null
+var pegChildren = null
 var pegId = null
+
+/*
+pegChildren[(pegChildren.length - 1)].id gives us the id value of the top-most disk on the stack
+currentDisk.id gives us the id of the disk we're holding to compare to the topmost disk of the chosen peg
+*/
+
 var selectPegId = function () {
-  pegId = this.id
-  moveDisk()
-}
+  /* if (currentDisk === null) {
+    return
+  } else { */
+    pegId = this.id
+    pegChildren = document.getElementById(pegId).children
+    topDiskId = pegChildren[(pegChildren.length - 1)].id
+    /* if (topDiskId < currentDisk.id) {
+      console.log('error')
+    } else { */
+      moveDisk(pegId, currentDisk)
+    }
+/*  }
+} */
 
 pegEvents()
 // Adds event listeners to pegs so their id can be saved in pegId for assignment.
 
-var moveDisk = function () {
+var moveDisk = function (pegId, currentDisk) {
   document.getElementById(pegId).appendChild(currentDisk)
+  pegId = null
+  currentDisk = null
 }
