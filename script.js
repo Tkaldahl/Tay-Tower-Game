eafb2507512603bb78d47a5e96693d1660198f4c
Taylor Kaldahl <tkaldahl90@gmail.com>
2018-05-30 18:06:15 -0400

    Game logic works

--- script.js
+++ script.js
@@ -14,14 +14,16 @@ var diskEvents = function () {
 }
 
 var currentDisk = null
+var currentDiskId = null
+var diskParentChildrenArray = null
 
 var selectDisk = function () {
-  currentDisk = this
-  // currentDisk = document.getElementById(topDiskId)
-  console.log(currentDisk)
+  diskParentChildrenArray = this.parentElement.children
+  currentDiskId = diskParentChildrenArray[(diskParentChildrenArray.length - 1)].id
+  currentDisk = diskParentChildrenArray.namedItem(currentDiskId)
 }
 diskEvents()
-// Adds event listeners to disks so they can be selected one at a time using the var currentDisk.
+// Adds event listeners to disks so that when clicked, they save the id of the top disk on their stack in var currentDiskId.
 
 const pegList = document.getElementsByClassName('peg')
 
@@ -34,32 +36,35 @@ var pegEvents = function () {
 var topDiskId = null
 var pegChildren = null
 var pegId = null
-
 /*
 pegChildren[(pegChildren.length - 1)].id gives us the id value of the top-most disk on the stack
 currentDisk.id gives us the id of the disk we're holding to compare to the topmost disk of the chosen peg
 */
+var moveDisk = function () {
+  document.getElementById(pegId).appendChild(currentDisk)
+}
 
 var selectPegId = function () {
-  /* if (currentDisk === null) {
+  pegChildren = this.children
+  topDiskId = pegChildren[(pegChildren.length - 1)].id
+  pegId = this.id
+  if (parseInt(topDiskId) > parseInt(currentDiskId)) {
+    moveDisk()
+  } else { console.log('error') }
+}
+/* if (currentDisk === null) {
     return
-  } else { */
-    pegId = this.id
-    pegChildren = document.getElementById(pegId).children
-    topDiskId = pegChildren[(pegChildren.length - 1)].id
-    /* if (topDiskId < currentDisk.id) {
+  } else {
+  pegId = this.id
+  pegChildren = document.getElementById(pegId).children
+  topDiskId = pegChildren[(pegChildren.length - 1)].id
+  /* if (topDiskId < currentDisk.id) {
       console.log('error')
-    } else { */
-      moveDisk(pegId, currentDisk)
-    }
-/*  }
+    } else {
+  moveDisk()
+}
+ }
 } */
 
 pegEvents()
 // Adds event listeners to pegs so their id can be saved in pegId for assignment.
-
-var moveDisk = function (pegId, currentDisk) {
-  document.getElementById(pegId).appendChild(currentDisk)
-  pegId = null
-  currentDisk = null
-}
